df26975e163772487d20486dc8b0f519f8db6db5
jamescarter2001 <jamesmcarter7@gmail.com>
2022-01-20 13:04:21 +0000

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -21,14 +21,12 @@ project "Pagoda"
 	pchheader "pgpch.h"
 	pchsource "Pagoda/src/Pagoda/pgpch.cpp"
 
-	files
-	{
+	files {
 		"%{prj.name}/src/**.h",
 		"%{prj.name}/src/**.cpp"
 	}
 
-	includedirs
-	{
+	includedirs {
 		"%{prj.name}/src",
 		"%{prj.name}/src/Pagoda",
 		"%{prj.name}/vendor/spdlog/include",
@@ -39,21 +37,21 @@ project "Pagoda"
 		staticruntime "On"
 		systemversion "latest"
 
-		defines
-		{
+		defines {
 			"PG_PLATFORM_WINDOWS",
-			"PG_ENABLE_ASSERTS",
-			"PG_DEBUG",
 			"SPDLOG_COMPILED_LIB"
 		}
 
-		postbuildcommands
+		--[[postbuildcommands
 		{
 			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/TestApp")
-		}
+		}--]]
 
 	filter "configurations:Debug"
-		defines "PG_DEBUG"
+		defines {
+		"PG_DEBUG",
+		"PG_ENABLE_ASSERTS"
+		}
 		buildoptions "/MDd"
 		symbols "On"
 
@@ -94,12 +92,6 @@ project "TestApp"
 		"Pagoda/vendor/spdlog/build/%{cfg.buildcfg}"
 	}
 
-	links
-	{
-		"Pagoda.lib",
-		"spdlogd.lib"
-	}
-
 	filter "system:windows"
 		cppdialect "C++17"
 		staticruntime "On"
@@ -112,18 +104,27 @@ project "TestApp"
 			"PG_DEBUG",
 			"SPDLOG_COMPILED_LIB"
 		}
+		
+		links
+		{
+		"Pagoda.lib",
+		"spdlogd.lib"
+		}
 
 	filter "configurations:Debug"
-		defines "CF_DEBUG"
+		defines {
+		"PG_DEBUG",
+		"PG_ENABLE_ASSERTS"
+		}
 		buildoptions "/MDd"
 		symbols "On"
 
 	filter "configurations:Release"
-		defines "CF_RELEASE"
+		defines "PG_RELEASE"
 		buildoptions "/MD"
 
 		optimize "On"
 	filter "configurations:Dist"
-		defines "CF_DIST"
+		defines "PG_DIST"
 		buildoptions "/MD"
 		optimize "On"
